90a0441647dfda1c73d71c1c63bcd3ef03fbf97b
Daniel Yak <danielyaa5@gmail.com>
2016-10-18 00:09:49 -0700

    update gitignore

--- src/routes/Project/reducers/ReducerTable.js
+++
@@ -1,6 +0,0 @@
-const initialState = 0
-export default function counterReducer (state = initialState, action) {
-  const handler = ACTION_HANDLERS[action.type]
-
-  return handler ? handler(state, action) : state
-}
