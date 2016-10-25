b9cb6a65f6d2f2922760224bfd154de4f6e60d7c
Daniel Yak <danielyaa5@gmail.com>
2016-10-24 23:02:35 -0700

    Removed templating project/index

--- src/routes/Project/index.js
+++ src/routes/Project/index.js
@@ -1,40 +1,13 @@
-import { injectReducer } from '../../store/reducers'
+import { injectReducer } from '../../store/reducers';
 
 export default (store) => ({
     path: 'projects/:id',
     getComponent(nextState, cb) {
-        require.ensure([], (require) => {
-            const Project = require('./components/ProjectView').default
+      require.ensure([], (require) => {
+          const Project = require('./components/ProjectView').default;
 
+          cb(null, Project);
 
-            cb(null, Project)
-
-        }, 'project')
+        }, 'project');
     }
-})
-
-
-import { injectReducer } from '../../store/reducers'
-
-export default (store) => ({
-  path : 'counter',
-  /*  Async getComponent is only invoked when route matches   */
-  getComponent (nextState, cb) {
-    /*  Webpack - use 'require.ensure' to create a split point
-     and embed an async module loader (jsonp) when bundling   */
-    require.ensure([], (require) => {
-      /*  Webpack - use require callback to define
-       dependencies for bundling   */
-      const Counter = require('./containers/CounterContainer').default
-      const reducer = require('./modules/counter').default
-
-      /*  Add the reducer to the store on key 'counter'  */
-      injectReducer(store, { key: 'counter', reducer })
-
-      /*  Return getComponent   */
-      cb(null, Counter)
-
-      /* Webpack named bundle   */
-    }, 'counter')
-  }
-})
+  });
