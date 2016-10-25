306c4d1e75d20e61aa8e2edf0ccf76d52cd413be
Daniel Yak <danielyaa5@gmail.com>
2016-10-24 22:57:52 -0700

    jcsc fix for projectview.js

--- src/routes/Project/components/ProjectView.js
+++ src/routes/Project/components/ProjectView.js
@@ -1,17 +1,17 @@
-import React from 'react'
+import React from 'react';
 
-import Table from './Table'
-import './ProjectView.scss'
+import Table from './Table';
+import './ProjectView.scss';
 
 export const ProjectView = (props) => {
-	console.log (props);
+  console.log(props);
 
-	return (
-	  <div>
+  return (
+    <div>
 	    <h1>Welcome to View for Project { props.params.id }!</h1>
 	    <Table />
 	  </div>
-	)
+  );
 };
 
-export default ProjectView
+export default ProjectView;
