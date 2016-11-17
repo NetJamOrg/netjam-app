dc6d6f5cafddc34ea911d2bc960fc4b7c32b9d91
Daniel Yak <danielyaa5@gmail.com>
2016-11-16 20:26:13 -0800

    one massive commit because I had to work on a different computer and transfer files

--- src/routes/Project/components/Clip.js
+++ src/routes/Project/components/Clip.js
@@ -5,20 +5,6 @@ import './Clip.scss';
 import common from 'common';
 
 export default class Clip extends Component {
-  addMenu(x, y) {
-    let clipMenu = document.getElementById('clip-menu');
-    clipMenu.style.left = common.numToPx(x);
-    clipMenu.style.top = common.numToPx(y);
-    clipMenu.dataset.clipId = this.props.clip.id;
-    clipMenu.dataset.track = this.props.clip.track;
-    clipMenu.style.display = 'flex';
-  }
-
-  handleContextMenu(e) {
-    e.preventDefault();
-    this.addMenu(e.clientX, e.clientY);
-  }
-
   componentDidMount() {
     let table = document.getElementById('table-component');
     let endPx = common.timeToPx(this.props.clip.endTime);
@@ -37,8 +23,7 @@ export default class Clip extends Component {
       <div className={ className }
            id={ `clip-component-${this.props.clip.id}` }
            data-track={ this.props.clip.track }
-           style={ clipStyle(this.props) }
-           onContextMenu={ this.handleContextMenu.bind(this) }>
+           style={ clipStyle(this.props) } >
       </div>
     );
   }
